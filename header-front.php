85fd03cc262e8788be5423a2d846286972f1e609
makklaud2 <makklaudtwo@gmail.com>
2022-11-28 23:50:06 +0200

    test commit

--- header-front.php
+++ header-front.php
@@ -4,9 +4,54 @@
     <title><?php echo wp_get_document_title(); ?></title>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
+
 </head>
 <body <?php body_class(); ?>>
-<header class="header" id="header"></header>
+<div class="wrapper">
+<header class="header" id="header">
+    <div class="header__head">
+        <div class="container">
+            <div class="header__head-wrapper">
+                <nav class="header__head-nav">
+                    <ul class="header__head-list">
+                        <li class="header__head-item"><a href="#" class="header__head-link">Goods Packaging</a></li>
+                        <li class="header__head-item"><a href="#" class="header__head-link">News and Articles</a></li>
+                        <li class="header__head-item"><a href="#" class="header__head-link">Promotions and Discounts</a></li>
+                        <li class="header__head-item"><a href="#" class="header__head-link">Payment and Delivery</a></li>
+                        <li class="header__head-item"><a href="#" class="header__head-link">Terms and Conditions</a></li>
+                    </ul>
+                </nav>
+                <div class="header__head-phone">
+                    <a href="tel:+97258-4797927" class="phone">+97258-4797927</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header__menu">
+        <div class="container">
+            <div class="header__menu-wrapper">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/artlogo.svg" alt="" class="header__menu-logo">
+                <nav class="header__menu-nav">
+                    <ul class="header__menu-list">
+                        <li class="header__menu-item"><a href="#" class="header__menu-link">GIFTS</a></li>
+                        <li class="header__menu-item"><a href="#" class="header__menu-link">KETUBAH</a></li>
+                        <li class="header__menu-item"><a href="#" class="header__menu-link">JEWISH ARTWORK</a></li>
+                        <li class="header__menu-item"><a href="#" class="header__menu-link">INSTITUTIONS AND ORGANIZATION</a></li>
+                        <li class="header__menu-item"><a href="#" class="header__menu-link">THE ILLUMINATED TORAH</a></li>
+                    </ul>
+                    <div class="header__menu-dot">.</div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/search.svg" alt=""
+                         class="header__menu-img">
+                </nav>
+                <div class="header__menu-lang">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/Eng.svg" alt="">
+                    <span>/</span>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/Isr.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
 <main class="main">
 <?php # TODO: code here ?>
--- templates/front-home.php
+++ templates/front-home.php
@@ -1,13 +1,16 @@
 <?php
 /*
  * Template name: Front Home Template
- */
+ */+
 
 get_header('front'); ?>
 
 <section class="section">
     <div class="container">
         <div class="section__wrapper">
+            <h1>MAIN</h1>
+            <h2>MAIN</h2>
+            <h3>MAIN</h3>
         </div>
     </div>
 </section>
