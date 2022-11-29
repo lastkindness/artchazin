a61782d5aa30a6e653ba29546236b07229ccd504
makklaud2 <makklaudtwo@gmail.com>
2022-11-29 02:48:47 +0200

    start hero section

--- templates/front-home.php
+++ templates/front-home.php
@@ -1,16 +1,54 @@
 <?php
 /*
  * Template name: Front Home Template
- */+
+ */
 
 get_header('front'); ?>
 
-<section class="section">
+<section class="hero">
     <div class="container">
-        <div class="section__wrapper">
-            <h1>MAIN</h1>
-            <h2>MAIN</h2>
-            <h3>MAIN</h3>
+        <div class="hero__wrapper">
+            <div class="hero__slider">
+                <div class="hero__slide">
+                    <img src="" alt="" class="hero__slide-img">
+                    <div class="hero__slide-content">
+                        <h2 class="hero__slide-title h2"></h2>
+                        <p class="hero__slide-descr"></p>
+                        <a href="" class="hero__slide-btn btn"></a>
+                    </div>
+                </div>
+<!--                <div class="hero__slide">
+                    <img src="" alt="" class="hero__slide-img">
+                    <div class="hero__slide-content">
+                        <h2 class="hero__slide-title h2"></h2>
+                        <p class="hero__slide-descr"></p>
+                        <a href="" class="hero__slide-btn btn"></a>
+                    </div>
+                </div>
+                <div class="hero__slide">
+                    <img src="" alt="" class="hero__slide-img">
+                    <div class="hero__slide-content">
+                        <h2 class="hero__slide-title h2"></h2>
+                        <p class="hero__slide-descr"></p>
+                        <a href="" class="hero__slide-btn btn"></a>
+                    </div>
+                </div>
+                <div class="hero__slide">
+                    <img src="" alt="" class="hero__slide-img">
+                    <div class="hero__slide-content">
+                        <h2 class="hero__slide-title h2"></h2>
+                        <p class="hero__slide-descr"></p>
+                        <a href="" class="hero__slide-btn btn"></a>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </div>
+</section>
+<section class="top-slider">
+    <div class="container">
+        <div class="top-slider__wrapper">
+
         </div>
     </div>
 </section>
---
+++ templates/front-test.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Template name: Front Test Template
+ */
+
+get_header('front'); ?>
+
+<section class="section">
+    <div class="container">
+        <div class="section__wrapper">
+            <ul>
+                <li><div class="icon icon-doublearrow"></div></li>
+                <li><div class="icon icon-home"></div></li>
+                <li><div class="icon icon-search"></div></li>
+                <li><div class="icon icon-smallarrow"></div></li>
+                <li><div class="icon icon-arrowr"></div></li>
+                <li><div class="icon icon-mail"></div></li>
+                <li><div class="icon icon-mapdot"></div></li>
+                <li><div class="icon icon-telephone"></div></li>
+                <li><div class="icon icon-phone"></div></li>
+                <li><div class="icon icon-user"></div></li>
+                <li><div class="icon icon-facebook"></div></li>
+                <li><div class="icon icon-twitter"></div></li>
+                <li><div class="icon icon-youtube"></div></li>
+                <li><div class="icon icon-linkedin2"></div></li>
+            </ul>
+            <p><mark>Lorem ipsum dolor sit</mark> amet, consectetur adipisicing elit. Ab alias aliquid beatae consectetur doloribus eaque, enim fugiat in laboriosam libero magni mollitia natus neque non numquam qui rerum ullam voluptate?</p>
+            <blockquote cite="https://www.huxley.net/bnw/four.html">
+                <p>Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.</p>
+            </blockquote>
+            <figcaption>—Aldous Huxley, <cite>Brave New World</cite></figcaption>
+        </div>
+    </div>
+</section>
+
+<?php get_footer('front'); ?>
