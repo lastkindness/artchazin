51124a79684ae68157ba94aa450549009ee60c25
makklaud2 <makklaudtwo@gmail.com>
2022-12-30 19:15:40 +0200

    fix card grid response

---
+++ templates/front-about us.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Template name: Front About Us Template
+ */
+
+get_header('front'); ?>
+
+<section class="breadcrumbs">
+    <div class="container">
+        <div class="breadcrumbs__wrapper">
+            <div class="breadcrumbs__item"><a href="#" class="breadcrumbs__link"><span class="icon icon-home"></span></a></div>
+            <div class="breadcrumbs__item"><a href="#" class="breadcrumbs__link"><span class="icon icon-smallarrow"></span></a></div>
+            <div class="breadcrumbs__item active"><a href="#" class="breadcrumbs__link">About Us</a></div>
+        </div>
+    </div>
+</section>
+
+<section class="about">
+    <div class="container">
+        <div class="about__wrapper">
+            <div class="about__first">
+                <div class="about__descr">
+                    <div class="about__descr-title h1">About Art Chazin </div>
+                    <div class="about__descr-subtitle h2">Who we are ?</div>
+                    <div class="about__descr-divider"></div>
+                    <p class="about__desct-text">Art Chazin was founded in Jerusalem in 2000 by the artist and designer Rabbi Avraham Chazin, born into a family of artists and painters.
+                        Today Art Chazin is the leading company in the field of painting and creating original Jewish art on parchment. Included in this artwork is the Haggadah for Passover, written and drawn on parchment, the Megillat Esther, Jewish oil paintings and more. The company has gained a reputation among the world’s greatest Judaica collectors, pieces of "Art Chazin" have been shown at exhibitions around the world, and were purchased by highly discerning collectors for their beauty, originality and utmost attention to detail.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/about1.jpg" alt="" class="about__picture">
+            </div>
+            <div class="about__two">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/about2.jpg" alt="" class="about__img">
+                <div class="about__descr">
+                    <div class="about__descr-title h2">Guiding the Team of Artists</div>
+                    <div class="about__descr-divider"></div>
+                    <div class="about__descr-text">Our team is made up of 17 handpicked artists, guided by Avraham Chazin for several years, in order to produce the most magnificent and outstanding pictures of the highest level possible. These artists are extremely talented yet still totally dependent on the guidance and direction of Chazin. Before they can draw a scene, Chazin draws a sketch for them from which they can build the artwork. Avraham Chazin coaches them through every detail- every character, every movement and every colour scheme. This system enables Chazin to produce the final product, which is breathtakingly beautiful, and higher than any global standard.</div>
+                    <div class="about__descr-title h2">Our Gallery in Jerusalem</div>
+                    <div class="about__descr-divider"></div>
+                    <div class="about__descr-text">Art Chazin's prestigious gallery in the King David Area, in Jerusalem, which exsisted until 2012, featured its works and was intended as an exhibition for all collectors from around the globe. Due to the massive renovation and construction work in the region, and added to that the global economic situation, the company decided to temporarily close the gallery. </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer('front'); ?>
