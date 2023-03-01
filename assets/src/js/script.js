174a23a19f4380f29b94e491d425b10d7afeb845
makklaud2 <makklaudtwo@gmail.com>
2023-03-01 17:12:48 +0200

    commit

---
+++ assets/src/js/script.js
@@ -0,0 +1,7 @@
+import import_js from './app.js';
+import '../scss/styles.scss';
+import bannerphp_block from '../../../acf-blocks/banner.php/js/app.js';
+import callbackphp_block from '../../../acf-blocks/callback.php/js/app.js';
+import herophp_block from '../../../acf-blocks/hero.php/js/app.js';
+import topsliderphp_block from '../../../acf-blocks/top_slider.php/js/app.js';
+import wallphp_block from '../../../acf-blocks/wall.php/js/app.js';
